cd2d654535432a30c6735f45c51d69c3d0c3c945
Mahelvson Chaves <mahelvson@gmail.com>
2025-04-01 14:40:21 -0300

    include models and fixing cat bugs

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
+from OpenGL.GL import *
+import copy
 import pygame
 import datetime
 from pygame.locals import *
-from OpenGL.GL import *
 import pygame.freetype
 
 from player import Player
-from maze import load_textures
+from maze import load_textures, cat_model
 from cat import Cat
 from renderer import setup_opengl, render_scene
 from config import WIDTH, HEIGHT, MAZE, CHEESE_POSITIONS
 
 def main():
+    maze = copy.deepcopy(MAZE)
+    cheese_positions = list(CHEESE_POSITIONS)
     pygame.init()
     pygame.freetype.init()
     font = pygame.freetype.SysFont(None, 32)
     screen = pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | OPENGL)
 
     setup_opengl()
-    load_textures()
+    cat_texture = load_textures()
     player = Player()
     
     # cria os gatos e indica as posicoes em que eles andam
     cats = [
-        Cat((1, 5), (3, 5)),
-        Cat((5, 1), (5, 3)),
+        Cat((1, 5), (2, 5), cat_model, cat_texture=cat_texture, rotate_angle=0),
+        Cat((5, 1), (5, 3), cat_model, cat_texture=cat_texture, rotate_angle=90),
     ]
 
     pygame.event.set_grab(True)
@@ -32,7 +35,9 @@ def main():
     collected = 0 # contador de queijos
     running = True
     start_ticks = pygame.time.get_ticks() # marca o tempo para o ranking
+    cheese_angle = 0 # angulo inicial do queijo
     while running:
+        cheese_angle = (cheese_angle + 1) % 360
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
@@ -44,15 +49,15 @@ def main():
 
         # logica da coleta de queijo
         player_pos = (player.x, player.y)
-        for cheese in CHEESE_POSITIONS[:]:  # lista atualizada das posicoes dos queijos - copia da inicializada
+        for cheese in cheese_positions[:]:  # lista atualizada das posicoes dos queijos - copia da inicializada
             dist = ((player_pos[0] - cheese[0])**2 + (player_pos[1] - cheese[1])**2) ** 0.5 
             if dist < .6: # distancia para haver coleta
-                CHEESE_POSITIONS.remove(cheese) # se pegou o queijo, retira ele do mapa
+                cheese_positions.remove(cheese) # se pegou o queijo, retira ele do mapa
                 collected += 1
                 mx, my = int(cheese[0]), int(cheese[1]) # pega o índice dele na matriz arredondando as coordenadas
                 MAZE[my][mx] = 0  # libera o caminho
 
-        if not CHEESE_POSITIONS: # verifica se tem queijo ainda e para o contador e o jogo, exibindo o ranking se nao houver
+        if not cheese_positions: # verifica se tem queijo ainda e para o contador e o jogo, exibindo o ranking se nao houver
             end_ticks = pygame.time.get_ticks()
             total_time = (end_ticks - start_ticks) / 1000  # segundos
             show_ranking(total_time)
@@ -64,7 +69,7 @@ def main():
                 show_message("O gato comeu o rato!")
                 return main()  # reinicia o jogo
 
-        render_scene(player, MAZE)
+        render_scene(player, maze, cheese_angle)
 
         # desenha os gatos
         for cat in cats:
@@ -73,7 +78,9 @@ def main():
         # contador de queijo
         glDisable(GL_LIGHTING)
         glDisable(GL_DEPTH_TEST)
+        glDisable(GL_TEXTURE_2D)
         draw_text(font, f"Queijos: {collected}/4", 10, 30)
+        glEnable(GL_TEXTURE_2D)
         glEnable(GL_LIGHTING)
         glEnable(GL_DEPTH_TEST)
 
@@ -120,15 +127,15 @@ def show_ranking(player_time):
 
     parsed.sort(key=lambda x: x[1])
 
-    # Mostrar ranking final
+    # mostrar ranking final
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill((0, 0, 0))
     font = pygame.freetype.SysFont(None, 36)
 
     font.render_to(screen, (50, 40), "Ranking", (255, 255, 0))
 
-    # exibir o ranking baseado no top
-    for i, (date, tempo) in enumerate(parsed[:10]):  # Top 10 dos tempos
+    # exibir o ranking baseado no top 5
+    for i, (date, tempo) in enumerate(parsed[:5]):  # Top 5 dos tempos
         font.render_to(screen, (50, 100 + i * 40), f"{i+1}. {date} - {tempo:.2f} s", (255, 255, 255))
 
     font.render_to(screen, (50, 350), f"Seu tempo: {player_time:.2f} segundos", (0, 255, 0))
